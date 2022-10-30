4e32f79501a33e46a9c6371df98a06604b6c1ee0
Volodymyr Dovbyshchuk <vova2570604@gmail.com>
2022-10-30 10:48:17 +0200

    Cube

---
+++ Homework_2_Dovbyshchuk_Volodymyr/Level 2 tasks (Additional Points)/Task_3/Task_3/Program.cs
@@ -0,0 +1,14 @@
+﻿using Task_3;
+
+
+Cube cube1 = new Cube();
+
+Cube cube2 = new Cube(1, 2, 3);
+
+Cube cube3 = new Cube(3);
+
+cube3.Initialize();
+
+Console.WriteLine(cube3);
+
+Console.WriteLine(cube2.IsThroughHole());
